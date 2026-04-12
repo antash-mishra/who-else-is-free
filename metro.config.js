8cbcd580b6a859e8e3fbdf9005183aa4cffe5618
Sumit Narang <sumitnarang.web@gmail.com>
2026-04-12 20:59:45 +0100

    Redesign/home (#35)
    * Android fix for date time picker
    
    * Refactored splash screen
    
    * Redesign: Home - event card, section headers

--- metro.config.js
+++ metro.config.js
@@ -4,6 +4,7 @@ const config = getDefaultConfig(__dirname);
 
 config.transformer.babelTransformerPath = require.resolve('react-native-svg-transformer');
 config.resolver.assetExts = config.resolver.assetExts.filter((ext) => ext !== 'svg');
+config.resolver.assetExts.push('mp4');
 config.resolver.sourceExts.push('svg');
 
 module.exports = config;
